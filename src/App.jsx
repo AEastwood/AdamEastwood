c4f9fee6e94a0bf6dc131911172e406825c3c9a7
Adam Eastwood <adam-3@live.co.uk>
2023-11-23 16:10:52 +0000

    Small amendments

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,20 @@ function App() {
 
     return (
         <div
-            className="
-                flex
-                flex-col
-                gap-6
-                h-screen
-                bg-google/80
-                text-gray-100
-                justify-center
-                items-center
-                bg-repeat
-            "
+            className="flex flex-col gap-6 h-screen bg-google/80 text-gray-100 justify-center items-center bg-repeat"
         >
-
+            {/* Profile Picture */}
             <ProfilePicture/>
 
+            {/* My Name */}
             <span className="text-4xl xl:text-5xl font-semibold antialiased font-PlayFairDisplay tracking-wider">
                 Adam Eastwood
             </span>
 
+            {/* Contact Details */}
             <Contact/>
+
+            {/* Socials */}
             <Socials/>
         </div>
     )
